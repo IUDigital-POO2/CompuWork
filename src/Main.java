765b9e5fc2c5db850594de45226891b31c45e9db
Cristian Vargas <cristian.vargas@est.iudigital.edu.co>
2024-09-21 21:34:38 -0500

    Codificacion de las clases y creacion main para prueba  en rama Cristian-dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        public static void main(String[] args) {
+            // Crear algunos empleados
+            Empleado emp1 = new Empleado("Juan Pérez", 1, "Permanente");
+            Empleado emp2 = new Empleado("Ana López", 2, "Temporal");
+            Empleado emp3 = new Empleado("Carlos Ruiz", 3, "Permanente");
+
+            // Crear un departamento
+            Departamento departamentoIT = new Departamento("IT");
+
+            // Asignar empleados al departamento
+            departamentoIT.agregarEmpleado(emp1);
+            departamentoIT.agregarEmpleado(emp2);
+            departamentoIT.agregarEmpleado(emp3);
+
+            // Generar y mostrar reportes de desempeño individuales
+            System.out.println(ReporteDesempenio.generarReporte(emp1));
+            System.out.println(ReporteDesempenio.generarReporte(emp2));
+            System.out.println(ReporteDesempenio.generarReporte(emp3));
+
+            // Generar y mostrar reporte de desempeño del departamento IT
+            System.out.println("\nReporte de Desempeño del Departamento " + departamentoIT.getNombre() + ":");
+            System.out.println(ReporteDesempenio.generarReporte(departamentoIT));
+        }
 }
