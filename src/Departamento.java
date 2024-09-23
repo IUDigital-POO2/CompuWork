4a5dc5f6dbea64338d5b4370989408b7b1509c34
Juan Osorno <esteban.osorno@est.iudigital.edu.co>
2024-09-23 18:41:59 -0500

    Añadiendo la clase departamento

--- src/Departamento.java
+++ src/Departamento.java
@@ -1,2 +1,55 @@
 public class Departamento {
+    private String nombreDepartamento;
+    private String empleados;
+    private String descripcion;
+    private String jefe;
+
+    public Departamento(String nombreDepartamento, String empleados, String descripcion, String jefe) {
+        this.nombreDepartamento = nombreDepartamento;
+        this.empleados = empleados;
+        this.descripcion = descripcion;
+        this.jefe = jefe;
+    }
+
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    public String getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(String empleados) {
+        this.empleados = empleados;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getJefe() {
+        return jefe;
+    }
+
+    public void setJefe(String jefe) {
+        this.jefe = jefe;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" +
+                "nombreDepartamento='" + nombreDepartamento + '\'' +
+                ", empleados='" + empleados + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", jefe='" + jefe + '\'' +
+                '}';
+    }
 }
