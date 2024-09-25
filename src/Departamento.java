f2842b869d86ce2f0bd7f4d51c0f292ffcbf9584
Cristian Vargas <cristian.vargas@est.iudigital.edu.co>
2024-09-24 20:14:11 -0500

    Correcion clases en rama Cristian-dev

--- src/Departamento.java
+++ src/Departamento.java
@@ -2,43 +2,5 @@ import java.util.ArrayList;
 
 public class Departamento {
 
-    // Atributos
-    private String nombre;
-    private ArrayList<Empleado> empleados;
-
-    // Constructor
-    public Departamento(String nombre) {
-        this.nombre = nombre;
-        this.empleados = new ArrayList<>();
-    }
-    // Agregar un empleado al departamento
-    public void agregarEmpleado(Empleado empleado) {
-        empleados.add(empleado);
-    }
-
-    // Eliminar un empleado del departamento
-    public void eliminarEmpleado(int id) throws Exception {
-        for (Empleado e : empleados) {
-            if (e.getId() == id) {
-                empleados.remove(e);
-                return;
-            }
-        }
-        throw new Exception("Empleado no encontrado");
-    }
-
-    // Getters
-    public ArrayList<Empleado> getEmpleados() {
-        return empleados;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    // Setters
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
 public class Main {
         public static void main(String[] args) {
-            // Crear algunos empleados
-            Empleado emp1 = new Empleado("Juan Pérez", 1, "Permanente");
-            Empleado emp2 = new Empleado("Ana López", 2, "Temporal");
-            Empleado emp3 = new Empleado("Carlos Ruiz", 3, "Permanente");
-
-            // Crear un departamento
-            Departamento departamentoIT = new Departamento("IT");
-
-            // Asignar empleados al departamento
-            departamentoIT.agregarEmpleado(emp1);
-            departamentoIT.agregarEmpleado(emp2);
-            departamentoIT.agregarEmpleado(emp3);
-
             // Generar y mostrar reportes de desempeño individuales
-            System.out.println(ReporteDesempenio.generarReporte(emp1));
-            System.out.println(ReporteDesempenio.generarReporte(emp2));
-            System.out.println(ReporteDesempenio.generarReporte(emp3));
+            System.out.println("\nReporte de Desempeño de los Empleados:");
+            System.out.println(ReporteDesempenio.generarReporte(empleado1));
+            System.out.println(ReporteDesempenio.generarReporte(empleado2));
+            System.out.println(ReporteDesempenio.generarReporte(empleado3));
 
             // Generar y mostrar reporte de desempeño del departamento IT
-            System.out.println("\nReporte de Desempeño del Departamento " + departamentoIT.getNombre() + ":");
+            System.out.println("\nReporte de Desempeño del Departamento " + departamentoIT.getNombreDepartamento() + ":");
             System.out.println(ReporteDesempenio.generarReporte(departamentoIT));
         }
 }
