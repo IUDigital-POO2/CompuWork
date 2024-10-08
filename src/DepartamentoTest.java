ed8bee4dbb2e650435fa5283dc770303b92acf31
John Eduar <johneduar777@gmail.com>
2024-10-07 19:24:11 -0500

    Haciendo pruebas unitarias con JUnit en la clase Departamento

---
+++ src/DepartamentoTest.java
@@ -0,0 +1,54 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DepartamentoTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getNombreDepartamento() {
+    }
+
+    @Test
+    public void setNombreDepartamento() {
+    }
+
+    @Test
+    public void getDescripcion() {
+    }
+
+    @Test
+    public void setDescripcion() {
+    }
+
+    @Test
+    public void getJefe() {
+    }
+
+    @Test
+    public void setJefe() {
+    }
+
+    @Test
+    public void getEmpleados() {
+    }
+
+    @Test
+    public void setEmpleados() {
+    }
+
+    @Test
+    public void agregarEmpleado() {
+    }
+
+    @Test
+    public void eliminarEmpleado() {
+    }
+}
